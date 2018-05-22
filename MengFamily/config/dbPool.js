582cfee8616e16b23a27a7155d4ce1c72af3bcd3
DAEUN KIM <dek233@gmail.com>
2018-05-22 07:53:11 -0700

    Create Signup (Users)

--- MengFamily/config/dbPool.js
+++ MengFamily/config/dbPool.js
@@ -2,12 +2,12 @@ const mysql = require('promise-mysql');			// mysql 모듈의 promise 버전
 
 // rds 정보 입력 : hostname, username, password, default DB
 const dbConfig = {
-	host : 'hostname',
+	host : 'mengfamily-rds-mysql.cnokttxubju1.ap-northeast-2.rds.amazonaws.com',
 	port : 3306,
-	user : 'username',
-	password : 'password',
-	database : 'defaultDB',
-	connectionLimit : 20
+	user : 'root',
+	password : '11111111',
+	database : 'MengFamilty',
+	connectionLimit : 25
 };
 
 module.exports = mysql.createPool(dbConfig);	// connection pool을 module화
---
+++ MengFamily/routes/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+router.use('/signup', require('./signup.js'));	// ip:3000/signup
+router.use('/signin', require('./signin.js'));	// ip:3000/signin
+
+router.use('/', require('./board/index.js'));		// ip:3000/
+module.exports = router;
